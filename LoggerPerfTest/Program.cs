1405d99d6aa3d0cb30682c7cd906d3c4198d870e
Sylvain Taufour <4014315+staufour@users.noreply.github.com>
2018-09-24 19:14:20 +0200

    Initialize Logger per test

---
+++ LoggerPerfTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Common.Logging;
+
+namespace LoggerPerfTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger("PerfTest");
+            //var logger = LogManager.GetLogger("PerfTestCrash");
+            var log4mb = File.ReadAllText("4MB.txt");
+            var nb4mb = 1000;
+            long duration4mb;
+
+            var sw = new Stopwatch();
+
+            Console.WriteLine($"Start {nb4mb} 4MB logs.");
+
+            sw.Start();
+            for (int i = 0; i < nb4mb; i++)
+            {
+                logger.Info(log4mb);
+            }
+            sw.Stop();
+            duration4mb = sw.ElapsedMilliseconds;
+
+            Console.WriteLine($"{nb4mb} 4MB logs in {duration4mb}ms.");
+
+            var proc = Process.GetCurrentProcess();
+            Console.WriteLine($"PrivateMemorySize = {proc.PrivateMemorySize64}");
+            Console.WriteLine($"VirtualMemorySize = {proc.VirtualMemorySize64}");
+            Console.WriteLine($"PeakVirtualMemorySize = {proc.PeakVirtualMemorySize64}");
+            Console.WriteLine($"PagedMemorySize = {proc.PagedMemorySize64}");
+            Console.WriteLine($"PeakPagedMemorySize = {proc.PeakPagedMemorySize64}");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
